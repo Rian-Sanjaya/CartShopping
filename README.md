4b2ce587f220b37ba839548ddf5c390aaa38022b
Rian Sanjaya <rian.sj@gmail.com>
2019-02-19 18:45:38 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Sirclo Technical Screening Test
+
+## Language: javascript
+
+## To test run 
+
+- copy all fivaa.js
+- paste into google chrome console
+- press enter
---
+++ ShoppingCart.js
@@ -0,0 +1,36 @@
+class Cart {
+    cart = {};
+
+    tambahProduk(kodeProduk, kuantitas) {
+        if (kodeProduk && kuantitas) {
+            this.cart[kodeProduk] = (this.cart[kodeProduk] || 0) + kuantitas;
+        }
+    }
+
+    hapusProduk(kodeProduk) {
+        if (this.cart[kodeProduk]) delete this.cart[kodeProduk];
+    }
+
+    tampilkanCart() {
+        const key = Object.keys(this.cart);
+        key.forEach( k => {
+            console.log(k + ' (' + this.cart[k] + ')');
+        });
+    }
+}
+
+keranjang = new Cart();
+
+keranjang.tambahProduk("Topi Putih", 2);
+
+keranjang.tambahProduk("Kemeja Hitam", 3);
+
+keranjang.tambahProduk("Sepatu Merah", 1);
+keranjang.tambahProduk("Sepatu Merah", 4);
+keranjang.tambahProduk("Sepatu Merah", 2);
+
+keranjang.hapusProduk("Kemeja Hitam");
+
+keranjang.hapusProduk("Baju Hijau");
+
+keranjang.tampilkanCart();
